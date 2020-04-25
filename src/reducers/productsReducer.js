27ce42affb145489c5bf3493e68d33011b747058
Diego Taveras <diegotaveras2@outlook.com>
2020-04-25 01:09:52 -0400

    Setting up Redux and adding redux dev tools

---
+++ src/reducers/productsReducer.js
@@ -0,0 +1,15 @@
+// each reducer has his one state
+
+const initialState = {
+    products: [],
+    error: null,
+    loading: false
+}
+
+export default function( state = initialState, action){
+    switch (action.type) {
+        
+        default:
+            return state;
+    }
+}
