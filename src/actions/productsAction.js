6cef3de865a1350ab5ba7814428054bc945402b6
Diego Taveras <diegotaveras2@outlook.com>
2020-04-25 03:39:43 -0400

    Inserting producst to API

--- src/actions/productsAction.js
+++ src/actions/productsAction.js
@@ -1,17 +1,23 @@
 import{
+
     ADD_PRODUCT,
     ADD_PRODUCT_ERROR,
     ADD_PRODUCT_SUCCES
 } from '../types'
+import axiosClient from '../config/axios';
+
 
 // Create new Products
 export function createNewProductAction(product) {
-    return (dispatch) => {
+    return async (dispatch) => {
         dispatch( addProduct() );
 
         try {
+            // insert on API
+            await axiosClient.post('/productos', product)
             dispatch( addProductSuccess(product))
         } catch (error) {
+            console.log(error)
             dispatch( addProductError(true))
         }
     }
