c5de744ae5f47856a4adbaa046e921db93b5bfd4
Diego Taveras <diegotaveras2@outlook.com>
2020-04-25 00:10:30 -0400

    add routing,New Product and edit product component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React,{Fragment} from 'react';
-import Header from './Components/Header';
+import React,{} from 'react';
+import Header from './components/Header';
+import Products from './components/Products';
+import NewProduct from './components/NewProduct';
+import EditProduct from './components/EditProduct';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+
+
 
 
 function App() {
   return(
-    <Fragment>
+    <Router>
         <Header />
-    </Fragment>
+        <div className="container mt-5">
+          <Switch>
+              <Route exact path="/" component={Products}/> 
+              <Route exact path="/products/new" component={NewProduct}/>
+              <Route exact path="/products/edit/:id" component={EditProduct}/>
+          </Switch>
+        </div>
+      
+    </Router>
   )
 }
 
---
+++ src/Components/NewProduct.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const NewProduct = () => {
+    return (
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-bold">
+                            Add New Product
+                        </h2>
+                        <form action="">
+                            <div className="form-group">
+                                <label htmlFor="">Name of product</label>
+                                <input 
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Name of product"
+                                    name="name"
+                                />
+                            </div>
+
+                            <div className="form-group">
+                            <label htmlFor="">Price of product</label>
+                                <input 
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Price of product"
+                                    name="precio"
+                                />
+                            </div> 
+
+                            <button
+                                type="submit"
+                                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                            >
+                            Add
+                            </button>
+                            
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NewProduct
